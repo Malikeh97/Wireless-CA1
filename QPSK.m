f0f39765f4ce378039f82b06b810b0a015da0fa1
Malikeh97 <malikehehghaghi@gmail.com>
2019-04-28 10:24:48 +0430

    QPSK scatter plot done

--- QPSK.m
+++ QPSK.m
@@ -17,9 +17,9 @@ title('QPSK constellation without noise');
 xlabel('real part');
 ylabel('imaginary part');
 snrd = 10; %// in dB
-snrn = 10^(snrd/10)
-h=1/sqrt(2)*(randn(num_symbols,1)+1i*randn(num_symbols,1)); %N(0,1)
-% h=1/sqrt(2)*(normrnd(0,1)+1i*normrnd(0,1)); %test
+snrn = 10.^(snrd./10)
+% h=1/sqrt(2)*(randn(num_symbols,1)+1i*randn(num_symbols,1)); %N(0,1)
+h=1/sqrt(2)*(normrnd(0,1)+1i*normrnd(0,1)); %test
 n=1/sqrt(2)*(sqrt(1/snrn)*randn(num_symbols,1)+1i*sqrt(1/snrn)*randn(num_symbols,1)); %N(0,sigma^2)
 % rx_sig = awgn(h*tx_sig, snr, 'measured');
 hh=rot90(fliplr(h),-1);
